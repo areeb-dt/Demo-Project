3842d38105ee1473e3fa3fca22d69d5327cbe6c2
Areeb <areeb@pcs-macbook-pro.gateway.pace.com>
2014-03-07 10:45:20 -0800

    Initial Commit

---
+++ AreebList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  AreebList
+//
+//  Created by Areeb on 3/7/14.
+//  Copyright (c) 2014 Areeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
