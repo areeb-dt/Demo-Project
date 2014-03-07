32f17caa80291f90542193d759d41bc429f3701f
areeb-dt <areeb@pcs-macbook-pro.gateway.pace.com>
2014-03-07 13:05:52 -0800

    Navigating between scenes

---
+++ AreebList/XYZAddToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  XYZAddToDoItem.h
+//  AreebList
+//
+//  Created by Areeb on 3/7/14.
+//  Copyright (c) 2014 Areeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAddToDoItem : UIViewController
+
+@end
---
+++ AreebList/XYZToDoList.h
@@ -0,0 +1,13 @@
+//
+//  XYZToDoList.h
+//  AreebList
+//
+//  Created by Areeb on 3/7/14.
+//  Copyright (c) 2014 Areeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZToDoList : UITableViewController
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+@end
